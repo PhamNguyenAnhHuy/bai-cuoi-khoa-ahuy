ad25b3e094ee33b9f3f4a9fe4293910b789eeb56
PhamNguyenAnhHuy <phamanhhuy2k8@gmailcom>
2024-05-25 15:59:37 +0700

    update slideshow, hinh anh, noi dung v.0.1

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function SignIn() {
         var checkObj = JSON.parse(checkJSON);
         if(ndMkLogin == checkObj.password){
             alert("Congratulation!");
-        window.location = "./index.html";
+        window.location = "./Begin.html";
         }
         else{
             alert("Sign in fail!")
@@ -52,3 +52,56 @@ function SignIn() {
 function SignOut(){
     window.location.href = "login.html"
 }
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+const searchInput = document.getElementById("search-input");
+const cityName = document.querySelector(".city-name");
+const weatherState = document.querySelector(".weather-state");
+const weatherIcon = document.querySelector(".weather-icon");
+const temperature = document.querySelector(".temperature");
+const sunrise = document.querySelector(".sunrise");
+const sunset = document.querySelector(".sunset");
+const humidity = document.querySelector(".humidity");
+const windSpeed = document.querySelector(".wind-speed");
+
+const API_KEY ="e5f09c8f6b6b9f0bb90fd299f434ecef";
+const DEFAULT_VALUE = "--";
+
+searchInput.addEventListener("change", (event) =>{
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&APPID=${API_KEY}&units=metric&lang=kr`)
+    .then(response => response.json())
+    .then((data)=>{
+        cityName.innerHTML = data.name || DEFAULT_VALUE;
+        weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+        temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
+        console.log(data);
+    })
+});
